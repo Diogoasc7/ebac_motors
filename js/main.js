873b7b1bfe369aeaf10f3651ddcfa30a1992bd04
Diogoasc7 <diogodsc1456@gmail.com>
2024-07-20 14:13:41 -0300

    Algumas explicações foram adicionadas

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ $(document).ready(function(){ // Garante que todo o código dentro seja executad
         $('#veiculoInteresse').val(nomeVeiculo);
 
         $('html').animate({
-            scrollTop: destino.offset().top
+            scrollTop: destino.offset().top //Essa função retorna as coordenadas em relação ao documento. Se você deseja obter as coordenadas em relação ao elemento pai mais próximo com uma posição definida, você pode usar a função position() em vez de offset(). A função position() retorna as coordenadas de posicionamento em relação ao elemento pai.
         }, 1000)
     })
 });
@@ -68,3 +68,22 @@ Ao clicar em um botão, o campo "Veículo de interesse" no formulário de contat
 A página é rolada suavemente até a seção de contato (#contato) em 1 segundo (1000 milissegundos).
 */
 
+/*PLUGINS JEQUERY*/
+/*
+    https://igorescobar.github.io/jQuery-Mask-Plugin/ -> Plugin de máscara
+    https://jqueryvalidation.org/ -> Plugin de validação
+    https://kenwheeler.github.io/slick/ -> Plugin para carrocel e CSS
+*/
+
+/*OUTROS*/
+/*
+    jQuery UI: É uma coleção de plugins e componentes de interface do usuário que adicionam recursos avançados de interação, como efeitos visuais, arrastar e soltar, seleção, redimensionamento e muito mais. 
+
+    • Magnific Popup: É um plugin para exibir imagens, vídeos e conteúdo em janelas pop-up com uma ampla gama de opções de personalização, como zoom, galerias e recursos avançados de controle de pop-up. 
+
+    • FullCalendar: É um plugin de calendário completo e altamente configurável, que permite exibir e gerenciar eventos em um calendário interativo. Ele oferece suporte a arrastar e soltar, visualização de mês/semana/dia, personalização de eventos e muito mais.
+
+    • Chosen: É um plugin para melhorar a experiência de seleção de elementos <select>. Ele transforma caixas de seleção simples em elementos avançados de seleção com pesquisa, filtragem, seleção múltipla e outras opções de personalização. 
+
+    • DataTables: É um plugin para criar tabelas interativas e responsivas, permitindo filtrar, classificar e pesquisar dados de forma avançada. Ele suporta recursos como paginação, ordenação, pesquisa em tempo real, exportação de dados e muito mais.
+*/
